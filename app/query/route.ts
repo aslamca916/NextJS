cdeb7f7ed4b66f4ff39867fd78c5fab274e3fd5c
aslamca916 <aslamca1998@gmail.com>
2025-01-02 12:07:19 +0530

    second commit

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,31 @@
-// import { db } from "@vercel/postgres";
+import { db } from "@vercel/postgres";
 
-// const client = await db.connect();
+// import postgres from "postgres";
+const DB_URL = `${process.env.POSTGRES_URL}`
+console.log("DB URL",DB_URL);
 
-// async function listInvoices() {
-// 	const data = await client.sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
 
-// 	return data.rows;
-// }
+// const client = {
+//   sql: postgres(DB_URL),
+// };
+const client = await db.connect();
+
+async function listInvoices() {
+	const data = await client.sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
+
+	return data;
+}
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+  
+  try {
+  	return Response.json(await listInvoices());
+  } catch (error) {
+  	return Response.json({ error }, { status: 500 });
+  }
 }
